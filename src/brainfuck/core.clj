6102a2241b9ad02a27bea7d0ba7fa80fc1e8b1ab
Alex Nichol <unixpickle@gmail.com>
2015-11-04 14:52:57 +0000

    split interpreter into different namespace

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -1,53 +1,6 @@
 (ns brainfuck.core)
-
-(defn- matching-bracket
-  [tokens startIndex direction]
-  (loop [i startIndex c 0]
-    (let [chr (get tokens i)]
-      (assert (not (= chr nil)))
-      (cond (= chr \[) (if (= c -1) (inc i) (recur (direction i) (inc c)))
-            (= chr \]) (if (= c 1) (inc i) (recur (direction i) (dec c)))
-            :else (recur (direction i) c)))))
-
-(defn- matching-close
-  [tokens openIndex]
-  (matching-bracket tokens openIndex inc))
-
-(defn- matching-open
-  [tokens closeIndex]
-  (matching-bracket tokens closeIndex dec))
-
-(defn- write-memory
-  [memory pointer v]
-  (concat (take pointer memory) (list v) (drop (inc pointer) memory)))
-
-(defn- add-memory
-  [m p s]
-  (write-memory m p (+ s (nth m p))))
-
-(defn- run-machine
-  [tokens ip memory pointer input]
-  (let [inst (get tokens ip)]
-    (cond (= inst nil) nil
-          (= inst \>) (recur tokens (inc ip) memory (inc pointer) input)
-          (= inst \<) (recur tokens (inc ip) memory (dec pointer) input)
-          (= inst \+) (recur tokens (inc ip) (add-memory memory pointer 1) pointer input)
-          (= inst \-) (recur tokens (inc ip) (add-memory memory pointer -1) pointer input)
-          (= inst \.) (do
-                        (print (char (nth memory pointer)))
-                        (recur tokens (inc ip) memory pointer input))
-          (= inst \,) (if (empty? input)
-                          (recur tokens (inc ip) (write-memory memory pointer 0) pointer input)
-                          (let [c (int (first input)) ni (rest input)]
-                            (recur tokens (inc ip) (write-memory memory pointer c) pointer ni)))
-          (= inst \[) (if (= 0 (nth memory pointer))
-                          (recur tokens (matching-close tokens ip) memory pointer input)
-                          (recur tokens (inc ip) memory pointer input))
-          (= inst \]) (if (= 0 (nth memory pointer))
-                          (recur tokens (inc ip) memory pointer input)
-                          (recur tokens (matching-open tokens ip) memory pointer input))
-          :else (recur tokens (inc ip) memory pointer input))))
+(require 'brainfuck.interpreter)
 
 (defn -main
   [program & input]
-  (run-machine (slurp program) 0 (repeat 0) 0 (apply str input)))
+  (brainfuck.interpreter/run-machine (slurp program) (apply str input)))
