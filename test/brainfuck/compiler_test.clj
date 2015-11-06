85d10eb7260ef6e9bdff388ffa2ea109d6c3d23c
Alex Nichol <unixpickle@gmail.com>
2015-11-06 16:25:52 -0500

    generalize and cleanup regs test

--- test/brainfuck/compiler_test.clj
+++ test/brainfuck/compiler_test.clj
@@ -7,15 +7,36 @@
 (def reg-dec @#'brainfuck.compiler/reg-dec)
 (def reg-to-scratch #'brainfuck.compiler/reg-to-scratch)
 
+(def initial-state
+  (let [regs (apply concat (repeat reg-count '(0 256 1 2)))
+        prefix (repeat (+ 4 scratch-size) 0)
+        suffix '(0 2)]
+    (concat prefix regs suffix)))
+
+(defn state-set-scratch
+  [state idx val]
+  (seq (assoc (vec state) idx val)))
+
+(defn state-set-reg
+  [state idx val]
+  (let [regIdx (+ 4 scratch-size (* 4 idx))]
+    (seq (assoc (vec state) regIdx val (inc regIdx) (- 256 val)))))
+
+(defn states-equal
+  [s1 s2]
+  (cond (< (count s1) (count s2)) (states-equal (concat s1 [0]) s2)
+        (> (count s1) (count s2)) (states-equal s2 s1)
+        :else (= (seq s1) (seq s2))))
+
+(defn are-states-equal
+  [s1 s2]
+  (dorun (map #(is (states-equal %1 %2)) s1 s2)))
+
 (deftest initialize-state-test
   (testing "initialize-state"
-    (let [{:keys [memory pointer]} (run-machine initialize-state "")
-          regs (apply concat (repeat reg-count '(0 256 1 2)))
-          null-preamble (repeat (+ 4 scratch-size) 0)
-          suffix '(0 2)
-          expected-memory (concat null-preamble regs suffix)]
+    (let [{:keys [memory pointer]} (run-machine initialize-state "")]
       (is pointer (+ scratch-size (* (inc reg-count) 4)))
-      (is (= expected-memory (take (count expected-memory) memory))))))
+      (states-equal memory initial-state))))
 
 (deftest reg-inc-dec-test
   (testing "reg-inc/reg-dec"
@@ -28,25 +49,17 @@
                               (str reg-dec reg-dec)
                               (apply str (repeat 256 reg-dec))
                               (apply str (repeat 259 reg-dec))))
-          prefix (repeat (+ 4 scratch-size) 0)
-          regs (apply concat (repeat (dec reg-count) '(0 256 1 2)))
-          suffix '(1 2 0 2)
-          expected (map #(concat prefix regs (list % (- 256 %)) suffix)
-                        '(1 2 0 3 255 254 0 253))
-          mem-len (+ 4 scratch-size (* (inc reg-count) 4))
-          actual (map #(take mem-len (:memory (run-machine % ""))) programs)]
-      (dorun (map #(is (= %1 %2)) actual expected)))))
+          actual (map #(:memory (run-machine % "")) programs)
+          expected (map #(state-set-reg initial-state (dec reg-count) %)
+                        [1 2 0 3 255 254 0 253])]
+      (are-states-equal actual expected))))
 
 (deftest reg-to-scratch-test
   (testing "reg-to-scratch"
     (let [s0 [0 1 2 2] s1 [1 2 0 1]
           init (str initialize-state "<<<<" (apply str (repeat 100 reg-inc)))
           programs (map #(str init (reg-to-scratch %1 %2)) s0 s1)
-          scratchRegs (map #(assoc [0 0 0] % 100) s0)
-          prefixes (map #(concat %1 (repeat (inc scratch-size) 0)) scratchRegs)
-          normalRegs (apply concat (repeat (dec reg-count) '(0 256 1 2)))
-          body (concat normalRegs [100 156 1 2 0 2])
-          mem-len (+ 4 scratch-size (* (inc reg-count) 4))
-          expected (map #(concat % body) prefixes)
-          actual (map #(take mem-len (:memory (run-machine % ""))) programs)]
-      (dorun (map #(is (= %1 %2)) actual expected)))))
+          before (state-set-reg initial-state (dec reg-count) 100)
+          expected (map #(state-set-scratch before % 100) s0)
+          actual (map #(:memory (run-machine % "")) programs)]
+      (are-states-equal actual expected))))
