a580e25ec58acfca65412e19a22fab6400e1c97f
Alex Nichol <unixpickle@gmail.com>
2015-11-06 14:47:46 -0500

    reg-dec tests pass

--- test/brainfuck/compiler_test.clj
+++ test/brainfuck/compiler_test.clj
@@ -4,6 +4,7 @@
             [brainfuck.interpreter :refer :all]))
 
 (def reg-inc @#'brainfuck.compiler/reg-inc)
+(def reg-dec @#'brainfuck.compiler/reg-dec)
 
 (deftest initialize-state-test
   (testing "initialize-state"
@@ -15,18 +16,22 @@
       (is pointer (+ scratch-size (* (inc reg-count) 4)))
       (is (= expected-memory (take (count expected-memory) memory))))))
 
-(deftest reg-inc-test
-  (testing "reg-inc basic"
+(deftest reg-inc-dec-test
+  (testing "reg-inc/reg-dec")
     (let [programs (map #(str initialize-state "<<<<" %)
                         (list reg-inc
                               (str reg-inc reg-inc)
                               (apply str (repeat 256 reg-inc))
-                              (apply str (repeat 259 reg-inc))))
-          prefix (concat (repeat (+ 4 scratch-size) 0))
+                              (apply str (repeat 259 reg-inc))
+                              reg-dec
+                              (str reg-dec reg-dec)
+                              (apply str (repeat 256 reg-dec))
+                              (apply str (repeat 259 reg-dec))))
+          prefix (repeat (+ 4 scratch-size) 0)
           regs (apply concat (repeat (dec reg-count) '(0 256 1 2)))
           suffix '(1 2 0 2)
           expected (map #(concat prefix regs (list % (- 256 %)) suffix)
-                        '(1 2 0 3))
+                        '(1 2 0 3 255 254 0 253))
           mem-len (+ 4 scratch-size (* (inc reg-count) 4))
           actual (map #(take mem-len (:memory (run-machine % ""))) programs)]
-      (dorun (map #(is (= %1 %2)) actual expected)))))
+      (dorun (map #(is (= %1 %2)) actual expected))))
