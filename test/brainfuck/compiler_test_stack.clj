5c60875e4e9a76f177adc95f528efdeb7bf50a0d
Alex Nichol <unixpickle@gmail.com>
2015-11-06 21:09:54 -0500

    pop-stack passes

--- test/brainfuck/compiler_test_stack.clj
+++ test/brainfuck/compiler_test_stack.clj
@@ -17,3 +17,25 @@
           pointer (:pointer result)]
         (is (= pointer (+ 4 scratch-size (* 4 (+ 5 reg-count)))))
         (is (states-equal actual expected)))))
+
+(deftest pop-stack-test
+  (testing "pop-stack"
+    (let [program (deep-str initialize-state
+                            (set-reg 0 100)
+                            (set-reg 1 17)
+                            (push-stack 0)
+                            (push-stack 1)
+                            (pop-stack 2)
+                            (pop-stack 3)
+                            (set-reg 4 20)
+                            (pop-stack 4))
+          expected (-> initial-state
+                       (state-set-reg 0 100)
+                       (state-set-reg 1 17)
+                       (state-set-reg 2 17)
+                       (state-set-reg 3 100))
+          result (run-machine program "")
+          actual (:memory result)
+          pointer (:pointer result)]
+      (is (= pointer (+ 4 scratch-size (* 4 (inc reg-count)))))
+      (is (states-equal actual expected)))))
