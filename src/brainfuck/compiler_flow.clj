2311046325cbecd70f749ffdf9870a2ba3d7b59a
Alex Nichol <unixpickle@gmail.com>
2015-11-07 13:24:21 -0500

    if-bf passes

--- src/brainfuck/compiler_flow.clj
+++ src/brainfuck/compiler_flow.clj
@@ -14,20 +14,31 @@
 
 (defn if-bf
   "If a block of \"condition\" returns a non-zero value, run if-body, otherwise
-   run else-body.
+   run an optional else-body.
    The if-body and else-body may modify any registers they please.
    Neither the if-body nor the else-body should leave the stack modified.
    The return value register may be affected at any time during an if statement."
-  [condition if-body else-body]
-  (str (set-reg return-value-reg 1)
-       (push-stack return-value-reg)
-       condition
-       (while-reg return-value-reg
-                  if-body
-                  (pop-stack return-value-reg)
-                  (set-reg return-value-reg 0)
-                  (push-stack return-value-reg))
-       (pop-stack return-value-reg)
-       (while-reg return-value-reg
-                  else-body
-                  (set-reg return-value-reg 0))))
+  ([condition if-body]
+   (str condition
+        (while-reg return-value-reg
+                   if-body
+                   (set-reg return-value-reg 0))))
+  ([condition if-body else-body]
+   (str (set-reg return-value-reg 1)
+        (push-stack return-value-reg)
+        condition
+        (while-reg return-value-reg
+                   if-body
+                   (pop-stack return-value-reg)
+                   (set-reg return-value-reg 0)
+                   (push-stack return-value-reg))
+        (pop-stack return-value-reg)
+        (while-reg return-value-reg
+                   else-body
+                   (set-reg return-value-reg 0)))))
+
+(defn return-reg
+  "Return the value of a register.
+   This is equivalent to moving the register into the return value register."
+  [reg]
+  (if (= reg return-value-reg) "" (mov-reg return-value-reg reg)))
--- test/brainfuck/compiler_test_flow.clj
+++ test/brainfuck/compiler_test_flow.clj
@@ -19,3 +19,22 @@
                        (state-set-reg 2 6))
           actual (:memory (run-machine program ""))]
       (is (states-equal actual expected)))))
+
+(deftest if-bf-test
+  (testing "if-bf"
+    (let [program (str initialize-state
+                       (set-reg 1 3)
+                       (if-bf (return-reg 1) (set-reg 2 1))
+                       (if-bf (return-reg 3) (set-reg 4 13))
+                       (if-bf (return-reg 3)
+                              (set-reg 1 1)
+                              (set-reg 1 2))
+                       (dec-reg 1)
+                       (if-bf (return-reg 1)
+                              (set-reg 1 7)
+                              (set-reg 1 9)))
+          expected (-> initial-state
+                       (state-set-reg 1 7)
+                       (state-set-reg 2 1))
+          actual (:memory (run-machine program ""))]
+      (is (states-equal actual expected)))))
